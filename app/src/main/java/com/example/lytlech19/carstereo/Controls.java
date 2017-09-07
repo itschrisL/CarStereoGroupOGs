f880d0c20f22af81cb836a20bbb91070a2363c92
Lytle <lytlech19@up.edu>
2017-09-07 16:25:42 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lytlech19/carstereo/Controls.java
@@ -0,0 +1,13 @@
+package com.example.lytlech19.carstereo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Controls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controls);
+    }
+}
